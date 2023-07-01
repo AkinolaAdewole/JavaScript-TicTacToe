b0cbecc06d831fbc2559bbd4ea89fb84a83af794
Akinola <adewoleakinola24@gmail.com>
2023-07-01 23:44:59 +0100

    More Logic

--- tic.js
+++ tic.js
@@ -1,8 +1,42 @@
 
 startGame=()=>{
     if(player1.value!=='' && player2.value!=="" && btn.value==="Start"){
+        btn.innerHTML="End"
         firstplayer.innerHTML= player1.value;
         secondplayer.innerHTML= player2.value;
+        gameplayer.innerHTML= player1.value + "start the game" + "{X}";
+        outcome.innerHTML="ENJOY";
+
+        one.innerHTML="";
+        two.innerHTML="";
+        three.innerHTML="";
+        four.innerHTML="";
+        five.innerHTML="";
+        six.innerHTML="";
+        seven.innerHTML="";
+        eight.innerHTML="";
+        nine.innerHTML="";
+    }else if (player1.value!=='' && player2.value!=="" && btn.value==="Start"){
+        player1.value = "";
+						player2.value = "";
+						firstplayer.innerHTML = "";
+						secondplayer.innerHTML = "";
+						gameplayer.innerHTML = "End of Game!!!";
+						outcome.innerHTML = "THANKS! THANKS!! THANKS!!!";
+
+						scoreone.innerHTML = "0";
+						scoretwo.innerHTML = "0";
+                        
+						btn.innerHTML = "Start";
+						one.innerHTML = "";
+						two.innerHTML = "";
+						three.innerHTML = "";
+						four.innerHTML = "";
+						five.innerHTML = "";
+						six.innerHTML = "";
+						seven.innerHTML = "";
+						eight.innerHTML = "";
+						nine.innerHTML = "";
     }
 }
 
