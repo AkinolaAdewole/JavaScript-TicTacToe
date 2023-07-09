83af55eef3b43f8a95967cb91ef6633aa1f123ad
Akinola <adewoleakinola24@gmail.com>
2023-07-09 23:03:43 +0100

    TikTakToe

--- tic.js
+++ tic.js
@@ -7,8 +7,8 @@ startGame=()=>{
 		check = true;
 		firstplayer.innerHTML = player1.value;
 		secondplayer.innerHTML = player2.value;
-		nameplayer.innerHTML = "Start the game " + player1.value + "{X}";
-		result.innerHTML = "Enjoy ur game. GOODLUCK!!!"
+		gamePlayer.innerHTML = "Start the game " + player1.value + "{X}";
+		outcome.innerHTML = "Enjoy ur game. GOODLUCK!!!"
 			one.innerHTML = "";
 			two.innerHTML = "";
 			three.innerHTML = "";
@@ -25,8 +25,8 @@ startGame=()=>{
 			player2.value = "";
 			firstplayer.innerHTML = "";
 			secondplayer.innerHTML = "";
-			nameplayer.innerHTML = "End of Game!!! THANKS";
-			result.innerHTML = "End of Game!!! THANKS";
+			gamePlayer.innerHTML = "End of Game!!! THANKS";
+			outcome.innerHTML = "End of Game!!! THANKS";
 			scoreone.innerHTML = "0";
 			scoretwo.innerHTML = "0";
 			btn.innerHTML = "Start";
@@ -40,8 +40,8 @@ startGame=()=>{
 			eight.innerHTML = "";
 			nine.innerHTML = "";
 		} else {
-			nameplayer.innerHTML = "Please input your names."; 
-			result.innerHTML = "Please input your names.";
+			gamePlayer.innerHTML = "Please input your names."; 
+			outcome.innerHTML = "Please input your names.";
 		}	
 }
 
@@ -57,9 +57,9 @@ startGame=()=>{
 				seven.innerHTML = "";
 				eight.innerHTML = "";
 				nine.innerHTML = "";
-				nameplayer.innerHTML = "GO ON PLAYERS!!!"
+				gamePlayer.innerHTML = "GO ON PLAYERS!!!"
 
-				result.innerHTML = "Another chance {GOODLUCK!!!}";
+				outcome.innerHTML = "Another chance {GOODLUCK!!!}";
 	}
 
 let initialvalue = "X"
@@ -68,8 +68,8 @@ const test =(e)=> {
    
 
 
-	if ((nameplayer.innerHTML == player1.value + " Won. Click on replay or End.") || (nameplayer.innerHTML == player2.value + " Won. Click on replay or End.")) {
-		nameplayer.innerHTML = "We have a winner already. Click on replay or End."
+	if ((gamePlayer.innerHTML == player1.value + " Won. Click on replay or End.") || (gamePlayer.innerHTML == player2.value + " Won. Click on replay or End.")) {
+		gamePlayer.innerHTML = "We have a winner already. Click on replay or End."
 	}
 
 
@@ -78,19 +78,19 @@ const test =(e)=> {
 	}
 	
 
-	if (nameplayer.innerHTML == "We have a winner already. Click on replay or End.") {
+	if (gamePlayer.innerHTML == "We have a winner already. Click on replay or End.") {
 		return;
 	} else if (check == true && box == " ") {
 		e.target.innerHTML = "X";
 		box = 1;
-		nameplayer.innerHTML = player2.value + " {O}"+ " your turn ";
+		gamePlayer.innerHTML = player2.value + " {O}"+ " your turn ";
 	} else if (check == true && box == 1) {
 		e.target.innerHTML = "O";
 		box = " ";
-		nameplayer.innerHTML = player1.value + " {X}"+ " your turn ";
+		gamePlayer.innerHTML = player1.value + " {X}"+ " your turn ";
 	} else {
-		nameplayer.innerHTML = "Click on start to start the game.";
-		result.innerHTML = "Click on start to start the game.";
+		gamePlayer.innerHTML = "Click on start to start the game.";
+		outcome.innerHTML = "Click on start to start the game.";
 	} 
 
    		// IF STATEMENT FOR WINNER (PLAYER) WITH LETTER X
